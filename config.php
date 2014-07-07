93ffd73be191d0113bea075f35c50c8effe2b95a
Alexander Labrador <globehackers@gmail.com>
2014-07-07 11:15:08 -0430

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $db_host="localhost";
-$db_usuario="root";
-$db_password="18667363120288hack";
-$db_nombre="eventos_db";
+$db_usuario="DEFINIR_USUARIO";
+$db_password="DEFINIR_CONTRASEÑA";
+$db_nombre="NOMBRE_DE_USUARIO";
 $conexion = @mysql_connect($db_host, $db_usuario, $db_password) or die(mysql_error());
 $db = @mysql_select_db($db_nombre, $conexion) or die(mysql_error());
 ?> 
