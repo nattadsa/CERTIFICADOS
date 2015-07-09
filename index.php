3f0e2587133855d299515a661c76d00aa85c4afa
Alexander Labrador <globehackers@gmail.com>
2015-07-09 10:48:07 -0430

    update 9 JUL 2015

--- index.php
+++ index.php
@@ -4,16 +4,14 @@
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	
 	<title>CERTIFICADOS - GUAYANALUG</title>
 	<meta name="description" content="">
 	<meta name="author" content="">
-	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
 	<link rel="shortcut icon" href="/favicon.ico">
 	<link rel="stylesheet" href="css/style.css?v=2">
 </head>
+
 <body>
 	<div id="container">
         <div id="contact-form" class="clearfix">
@@ -22,18 +20,13 @@
 
             <form method="post" action="certificado.php">
                 <label for="cedula">Cedula: <span class="required">*</span></label>
-                <input type="text" id="cedula" name="cedula" pattern="[0-9]+" placeholder="cedula de participante ejem: 12345678" required autofocus autocomplete='off' />
+                <input type="text" id="cedula" name="cedula" pattern="[0-9]{8}" placeholder="cedula de participante ejem: 12345678" required autofocus autocomplete='off' />
                           
                 <input type="submit" value="Crear" id="submit-button" />
                 <p id="req-field-desc"><span class="required">*</span> Todos los campos son requeridos.</p>
             </form>
         </div>
     </div>
-
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
 </body>
 </html>
-
-<!--   <label for="number">Cedula: <span class="required">*</span></label>
-                <input type="number" id="cedula" name="cedula" placeholder="V-12.345.678" required autofocus autocomplete='off' />
--->
