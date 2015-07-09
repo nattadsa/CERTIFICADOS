f2d46a84d69100154f93f5b9158ed55ac7c9845b
Alexander Labrador <globehackers@gmail.com>
2015-07-09 10:52:51 -0430

    update

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $db_host="localhost";
-$db_usuario="root";
-$db_password="18667363120288hack";
+$db_usuario="testuser";
+$db_password="testuser";
 $db_nombre="db_ven01";
 $conexion = @mysql_connect($db_host, $db_usuario, $db_password) or die(mysql_error());
 $db = @mysql_select_db($db_nombre, $conexion) or die(mysql_error());
