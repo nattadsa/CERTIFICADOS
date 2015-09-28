833026fb17b0ce237f59250ce6700d666119c2bc
Natanael <nattadsa@hotmail.com>
2015-09-28 20:03:32 -0300

    Update config.php
    mysql_connect cambio por mysqli_connect
    a partir de PHP 5.5 mysql_connect es obsoleta(deprecated)

--- config.php
+++ config.php
@@ -1,8 +1,7 @@
 <?php
-$db_host="localhost";
-$db_usuario="testuser";
-$db_password="testuser";
-$db_nombre="db_ven01";
-$conexion = @mysql_connect($db_host, $db_usuario, $db_password) or die(mysql_error());
-$db = @mysql_select_db($db_nombre, $conexion) or die(mysql_error());
+$hostname = '127.0.0.1';
+$user = 'testuser';
+$password = 'testuser';
+$database = 'db_ven01';
+$conexion = mysqli_connect($hostname, $user, $password, $database) or die("No se pudo establecer conección con el BD.");
 ?> 
